bac426b93969e19cf3981394072a2d6bf36370f6
Yeo <guswls9281@gmail.com>
2022-06-03 10:35:03 +0900

    회사유형 수정

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 from app_industry import run_industry
-from app_search import search_company, search_industry, search_jobtitle
+from app_search import search_industry, search_jobtitle
+from app_company_name import run_company_type, search_company
 
 def main():
     # 가로로 길게 보이도록 어케함?
@@ -10,7 +11,7 @@ def main():
 
     # st.set_page_config(layout="wide")
     st.sidebar.header('데이터 애널리스트 모집공고 분석')
-    column_list = ['연봉별','산업/섹터별','평가별','설립연도별','종합분석','검색']
+    column_list = ['회사/회사유형별','연봉별','산업/섹터별','평가별','설립연도별','종합분석','검색']
     
     choice = st.sidebar.radio(label= '분석 내용 선택',options = column_list)
     
@@ -20,9 +21,14 @@ def main():
 
            
     if choice == column_list[0]:
-        pass
+        name2 = st.text_input('회사명 검색',max_chars = 35)
+        if len(name2)>0:
+            search_company(name2)
+        run_company_type()
+        
     elif choice == column_list[1]:
         run_industry()
+   
     elif choice == column_list[2]:
         pass
     elif choice == column_list[3]:
@@ -30,6 +36,8 @@ def main():
     elif choice == column_list[4]:
         pass
     elif choice == column_list[5]:
+        pass
+    elif choice == column_list[6]:
         name = st.text_input('Job Title 키워드 검색',max_chars = 35)
         if len(name)>0:
             search_jobtitle(name)
--- app_company_name.py
+++ app_company_name.py
@@ -1,4 +1,36 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-def run_company_name():
-    pass
+df = pd.read_csv('data/df_data_anaylist2.csv', index_col=0)
+
+def search_company(name):
+    result = df.loc[df['Company Name'].str.lower().str.contains(name.lower()), ]
+    st.dataframe(result)
+    st.text('검색된 데이터는 {}개입니다.'.format(result.shape[0]))
+
+def run_company_type ():
+    
+    col1,col2 = st.columns([2,3])
+
+    ownership = df['Type of ownership'].value_counts()
+    ownership = ownership.to_frame()
+    ownership.columns = ['counts']
+ 
+    with st.expander('회사 유형 테이블'):
+        st.dataframe(ownership)
+    
+    chart_type = col1.radio('회사 유형 차트 확인',['원형 그래프','막대 그래프','버블 그래프'])
+    if chart_type == '원형 그래프':
+        fig1 = px.pie(ownership,names = ownership.index, values='counts')
+        st.plotly_chart(fig1)
+
+    elif chart_type == '막대 그래프':
+        fig2 = px.bar(ownership)
+        st.plotly_chart(fig2)
+
+    elif chart_type == '버블 그래프':
+        fig3 = px.scatter(ownership)
+        fig3.show()
+
+
